240a5804e9397a2d02551f67c80d8c00f22f6004
David Gatica <davgatik@gmail.com>
2020-12-31 12:58:19 -0600

    New Commit

--- src/components/Areas.js
+++ src/components/Areas.js
@@ -1,10 +1,71 @@
 import React, { Component } from 'react';
+import Header from './template/Header';
+import Sidebar from './template/SidebarEmpleado';
+import Footer from './template/Footer';
 
 class Areas extends Component {
+    constructor() {
+        super();
+        this.state = {
+            nombre: '',
+            descripcion: '',
+            estatus: false,
+            areas: [],
+            _id: ''
+        };
+    }
+    componentDidMount() {
+        this.fetchAreas();
+    }
+    
+    fetchAreas() {
+        const localhost = process.env.REACT_APP_LOCAL_HOST;
+        console.log(localhost);
+		fetch(localhost+'area/listarA') //por defecto es get
+			.then(res => res.json())
+			.then(data => {
+                console.log(data);
+				this.setState({areas:data.areas});
+                console.log(this.state.areas[0]);
+                console.log(this.state.areas);
+
+			});
+	}
+    
     render() {
         return (
             <div>
-                Areas
+            <Header/>
+            <Sidebar/>
+            <div className='content-wrapper'>
+
+                areas
+                <table border="solid">
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Descripción</th>
+                            <th>Estatus</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+
+                            this.state.areas.map(area =>  {
+                                return(
+                                    <tr key={area._id}>
+                                        <td>{area.nombre}</td>
+                                        <td>{area.descripcion}</td>
+                                        <td>{area.estatus}</td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
+            <Footer/>
+            
             </div>
         );
     }
--- src/components/Centros.js
+++ src/components/Centros.js
@@ -1,10 +1,79 @@
 import React, { Component } from 'react';
+import Header from './template/Header';
+import Sidebar from './template/SidebarEmpleado';
+import Footer from './template/Footer';
 
 class Centros extends Component {
+    constructor() {
+        super();
+        this.state = {
+            nombre: '',
+            telefono: '',
+            calle: '',
+            colonia: '',
+            cp: 0,
+            estatus: false,
+            centros: [],
+            _id: ''
+        };
+    }
+    componentDidMount() {
+        this.fetchCentros();
+    }
+    
+    fetchCentros() {
+        const localhost = process.env.REACT_APP_LOCAL_HOST;
+		fetch(localhost + 'centro/listarA') //por defecto es get
+			.then(res => res.json())
+			.then(data => {
+                console.log(data);
+				this.setState({centros:data.centros});
+                console.log(this.state.centros[0]);
+               // console.log(this.state.centros);
+
+			});
+	}
+    
     render() {
         return (
             <div>
-                Centros
+            <Header/>
+            <Sidebar/>
+            <div className='content-wrapper'>
+
+                centros
+                <table border="solid">
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Teléfono</th>
+                            <th>Calle</th>
+                            <th>Colonia</th>
+                            <th>CP</th>
+                            <th>Estatus</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+
+                            this.state.centros.map(centro =>  {
+                                return(
+                                    <tr key={centro._id}>
+                                        <td>{centro.nombre}</td>
+                                        <td>{centro.telefono}</td>
+                                        <td>{centro.calle}</td>
+                                        <td>{centro.colonia}</td>
+                                        <td>{centro.cp}</td>
+                                        <td>{centro.estatus}</td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
+            <Footer/>
+            
             </div>
         );
     }
